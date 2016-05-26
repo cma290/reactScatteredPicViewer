84c7822c3e3194a9cffcbefb1a68c28bfaa29e90
Cheng Ma <machengbeihang@gmail.com>
2016-05-25 19:05:25 -0700

    image components

--- src/components/Main.js
+++ src/components/Main.js
@@ -4,7 +4,7 @@ require('styles/App.css');
 import React from 'react';
 
 let imageData = require('../data/imageData.json');
-let yeomanImage = require('../images/yeoman.png');
+//let yeomanImage = require('../images/yeoman.png');
 
 function genImageURL(imageDataArr) {
 	for (let i = 0, j = imageDataArr.length; i < j; ++i) {
@@ -12,17 +12,40 @@ function genImageURL(imageDataArr) {
 		
 		imageDataItem.imageURL = require('../images/' + imageDataItem.fileName);
 		imageDataArr[i] = imageDataItem;
-		
-		return imageDataArr;
 	}
+	return imageDataArr;
 }
 imageData = genImageURL(imageData);
 
+var ImgFigure = React.createClass({
+	render: function () {
+		return (
+			<figure className="img-figure">
+				<img src={this.props.data.imageURL}
+					 alt={this.props.data.title}
+				/>
+				<figcaption>
+					<h2 className="img-title">{this.props.data.title}</h2>
+				</figcaption>
+			</figure>
+		);
+	}
+});
+
 class AppComponent extends React.Component {
   render() {
+  	var controllerUnits =[],
+  	imgFigures =[];
+
+  	imageData.forEach(function (value) {
+  		imgFigures.push(<ImgFigure data={value}/>);
+  	});
+
+
     return (
       <section className="stage">
       	<section className="img-sec">
+      	{imgFigures}
       	</section>
       	<nav className="controller-nav">
       	</nav>
